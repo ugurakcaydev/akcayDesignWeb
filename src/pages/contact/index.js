59d28096a0d3b45502123f5802fdaa552a34dfa2
TanerTube <tanerstar39@gmail.com>
2023-06-23 16:22:55 +0300

    iletişim sayfası %90 bitti

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -24,9 +24,9 @@ export default function Contact() {
                     <div className='rightComm'>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6023.774332213233!2d29.139539239811402!3d40.9839515178999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cacf5500589c07%3A0x5cc92203c718a23f!2sCemile%20Besler%20%C4%B0lkokulu!5e0!3m2!1str!2str!4v1687428287701!5m2!1str!2str" 
                         style={{border:"2"}} 
-                        allowfullscreen="true" 
+                        allowFullScreen={true}
                         loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
                 <div className='informationContainer'>
